cad8cb0f6921a84ae97b42efe978a5f2596319d5
kgf-cmd <Karolfracek123@gmail.com>
2024-02-03 20:24:52 +0100

    respowning

---
+++ scenes/Level/level.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+const ANIMAL = preload("res://scenes/animal/animal.tscn")
+@onready var animal_start = $AnimalStart
+ 
+func _ready():
+	add_animal()
+	SignalManager.on_animal_died.connect(add_animal)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func add_animal() -> void:
+	var animal = ANIMAL.instantiate()
+	animal.position = animal_start.position
+	add_child(animal)
