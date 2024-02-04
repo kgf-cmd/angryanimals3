a876316477854af4d871d6bea530b885ebe0eebd
kgf-cmd <Karolfracek123@gmail.com>
2024-02-04 15:05:19 +0100

    aniaml state

--- scenes/animal/animal.gd
+++ scenes/animal/animal.gd
@@ -1,14 +1,54 @@
 extends RigidBody2D
 
+enum ANIMAL_STATE {READY, DRAG, RELEASE}
 
-# Called when the node enters the scene tree for the first time.
+var _state: ANIMAL_STATE = ANIMAL_STATE.READY
+@onready var label = $Label
+
+
+
 func _ready():
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	pass
+	update(delta)
+	label.text = "%s" % ANIMAL_STATE.keys()[_state]
+
+	
+func set_new_state(new_state: ANIMAL_STATE) -> void:
+	_state = new_state
+	if _state == ANIMAL_STATE.RELEASE:
+		freeze = false
+	elif _state == ANIMAL_STATE.DRAG:
+		pass
+	
+	
+	
+	
+func detect_release() -> bool:
+	if _state == ANIMAL_STATE.DRAG:
+		if Input.is_action_just_released("drag") == true:
+			pass
+			return true
+	return false
+	
+func update_drag() -> void:
+	
+	if detect_release() == true:
+		return
+		
+	var gmp = get_global_mouse_position()
+	position = gmp
+
+func update(delta: float) -> void:
+	match _state:
+		ANIMAL_STATE.DRAG:
+			pass
+
+
+
 
 func die() -> void:
 	SignalManager.on_animal_died.emit()
@@ -16,3 +56,8 @@ func die() -> void:
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	die()
+
+
+func _on_input_event(viewport, event, shape_idx):
+	if _state == ANIMAL_STATE.READY and event.is_action_pressed("drag"):
+		set_new_state(ANIMAL_STATE.DRAG)
