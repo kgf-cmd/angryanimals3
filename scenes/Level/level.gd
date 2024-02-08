27948aa9e411da9cc35781b627efd278018df0ff
kgf-cmd <Karolfracek123@gmail.com>
2024-02-08 13:49:49 +0100

    levels

--- scenes/Level/level.gd
+++ scenes/Level/level.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 const ANIMAL = preload("res://scenes/animal/animal.tscn")
 @onready var animal_start = $AnimalStart
- 
+const MAIN = preload("res://main/main.tscn")
 func _ready():
 	add_animal()
 	SignalManager.on_animal_died.connect(add_animal)
@@ -10,7 +10,8 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if Input.is_key_pressed(KEY_ESCAPE):
+		get_tree().change_scene_to_packed(MAIN)
 
 func add_animal() -> void:
 	var animal = ANIMAL.instantiate()
