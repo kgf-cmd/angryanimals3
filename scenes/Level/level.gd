f42bde4071a0b7ad01efa7084f1497be228fc197
kgf-cmd <Karolfracek123@gmail.com>
2024-02-11 13:22:35 +0100

    polishing

--- scenes/Level/level.gd
+++ scenes/Level/level.gd
@@ -8,10 +8,6 @@ func _ready():
 	SignalManager.on_animal_died.connect(add_animal)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	if Input.is_key_pressed(KEY_ESCAPE):
-		get_tree().change_scene_to_packed(MAIN)
 
 func add_animal() -> void:
 	var animal = ANIMAL.instantiate()
